9d3fad36dc8470d94a462bd68bc59e354e209773
Khalid Safy <khalid_safy88@hotmail.com>
2021-10-20 20:03:22 +0100

    Added getImages function and event handler for search

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,10 +1,27 @@
-import react from "react";
+import react, { useState } from "react";
 import "../styles/Search.css";
+import getImages from "../requests/getImages";
 
 const Search = () => {
+  const [value, setValue] = useState("");
+  //   const [submit, setSubmit] = use
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    getImages(value);
+  };
+
   return (
     <>
-      <input className="search-input" type="text"></input>
+      <form onSubmit={handleSubmit}>
+        <input
+          className="search-input"
+          type="text"
+          onChange={(e) => setValue(e.target.value)}
+        ></input>
+        <button className="search-button" type="submit">
+          Search
+        </button>
+      </form>
     </>
   );
 };
