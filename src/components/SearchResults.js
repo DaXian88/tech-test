3ee41bce357e78e50f6650d1060f81bb9062c15e
Khalid Safy <khalid_safy88@hotmail.com>
2021-10-26 23:54:37 +0100

    Added some styling to image search results

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -2,16 +2,16 @@ import React from "react";
 import "../styles/SearchResults.css";
 
 const SearchResults = ({ searchResults }) => {
-  if (!searchResults) {
+  if (!searchResults.length) {
     return <p>No results found</p>;
   } else {
     return (
-      <>
+      <div className="image-container">
         <p>Search Results</p>
         {searchResults.map((result) => (
           <img className="search-image" src={result} alt="Space Pic" />
         ))}
-      </>
+      </div>
     );
   }
 };
